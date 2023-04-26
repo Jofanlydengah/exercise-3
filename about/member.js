ec0012281d658e5ebd92cabc1c5bd01453e1950d
Jofanlydengah <s2200066@student.unklab.ac.id>
2023-04-26 14:26:28 +0800

    first commit

---
+++ about/member.js
@@ -0,0 +1,4 @@
+module.exports.id = () => 'S2200666'
+module.exports.nim = () => '105022010029'
+module.exports.nama = () => 'Dengah,Jofanly Stevanus'
+module.exports.nomorTelp = () => '08114340789'
